eb2758368712a1db050169d29269a35b92a40b2a
juliokval <julesvalentin04@gmail.com>
2022-06-11 01:56:11 -0700

    Our Shell-Shell

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+extern char **environ;
+
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+int _strlen(const char *str);
+
+#endif
