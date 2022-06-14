c226e4d3bf7909696473a57bc09a162368ba40a7
juliokval <julesvalentin04@gmail.com>
2022-06-14 03:02:28 -0700

    Our Simple_Shell

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+/**
+  * authors: Kelly and Jules
+  * main - Entry point for our simple shell project created
+  *
+  * Return: 0 on success
+  */
+
