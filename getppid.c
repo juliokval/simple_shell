f3724d71efd570b2381737f7a87919f5ab2cb311
juliokval <julesvalentin04@gmail.com>
2022-06-15 14:37:37 -0700

    Our Simple-Shell

--- getppid.c
+++ getppid.c
@@ -1,22 +1,26 @@
-//program that prints the child and parent process id //
+/ program that prints the child and parent process id /
 
 #include <stdio.h>
 #include <unistd.h>
-
-int main()
+/**
+  * main - prints child and parent process
+  *
+  * Return: 0 for success
+  */
+int main(void)
 {
 	int p;
+
 	p = fork();
 
 	if (p < 0)
-	{
-		printf ("cannot be executed");
-	}
+		printf("cannot be executed");
 	else if (p == 0)
 	{
 		printf("child process id %d \n", getpid());
 	}
-	else {
+	else
+	{
 		printf("parent process id %d \n", getppid());
 	}
 	return (0);
